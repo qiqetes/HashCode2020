ae7dc3edbb93f8e9347fff3b7d081256bb722953
qiqetes <qiqetes@gmail.com>
2020-02-20 17:29:05 +0100

    First commit

---
+++ GoogleHashCode2020/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GoogleHashCode2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string projectPath = System.IO.Directory.GetCurrentDirectory().Substring(0, System.IO.Directory.GetCurrentDirectory().Length - 9);
+            string inputPath = projectPath + "\\inputData";
+            string outputPath = projectPath + "\\outputData";
+
+            string[] inputFileNames = new string[] { "b_small.in", "c_medium.in", "d_quite_big.in", "e_also_big.in" };
+
+
+            string line;
+            System.IO.StreamReader file = new System.IO.StreamReader(inputPath + "\\" + inputFileNames[3]);
+        }
+    }
+}
